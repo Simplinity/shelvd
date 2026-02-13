883db75db18a5752a50a337764fa0c5e7d7a3aae
Simplinity <bruno@simplinity.co>
2026-02-13 15:49:28 +0100

    chore: gitignore temp scripts

--- get-email.mjs
+++
@@ -1,8 +0,0 @@
-const RESEND_KEY = 're_C9F668Sw_GL9FgaWQDLXTbpDiE9EKyuMc'
-
-// Get recent emails from Resend
-const res = await fetch('https://api.resend.com/emails', {
-  headers: { 'Authorization': `Bearer ${RESEND_KEY}` }
-})
-const data = await res.json()
-console.log(JSON.stringify(data, null, 2))
